62a0c73c284446e409fa9767e1add94a583e39f5
syeopite <syeopite@syeopite.dev>
2021-06-27 11:27:15 -0700

    Overhaul md list create script and instance.yaml
    See https://github.com/iv-org/documentation/pull/76#issuecomment-864416290

--- src/create-instance-md.py
+++ src/create-instance-md.py
@@ -1,92 +1,156 @@
 """Extremely quick and dirty module for creating a markdown file from the instances.yaml file"""
 from urllib.parse import urlparse
 
+import pycountry
 import yaml
 from mdutils.mdutils import MdUtils
 
 
-def create_table(table_data, instance_data):
-    rows = []
-    for field, value in instance_data.items():
-        if value is None:
-            rows.append("")
-        
-        # Use markdown links for Addresses
-        elif field in ["url", "associated_clearnet_instance"]:
-            url = urlparse(value).hostname
-            rows.append(f"[{url}]({value})")
+class ColumnBuilder:
+    def route(self, name):
+        router = {
+            "url": self._create_url_column,
+            "associated_clearnet_instance": self._create_url_column,
+            "country": self._create_country_column,
+            "status": self._create_status_column,
+            "is_modified": self._create_modified_column,
+            "privacy_policy": self._create_privacy_policy_column,
+            "ddos_mitm_protection": self.ddos_protection,
+            "owner": self._create_owner_column
+        }
 
-        elif field == "status" and value is not None:
-            if value.get("display_content_is_image"):
-                rows.append(f"[![{value['display_content_image_fallback']}]({value['display_content']})]({value['url']})")
-            else:
-                rows.append(f"[{value['display_content']}]({value['url']})")
-        
-        elif field == "country" and value:
-            rows.append(f"{value['flag']} {value['name']}")
+        return router[name]
 
-        elif field == "modified":
-            if value.get("is_modified"):
-                rows.append(f"[Yes]({value['source_url']})")
-            else:
-                rows.append("No")
+    @staticmethod
+    def _create_url_column(url):
+        if not url:
+            return ""
 
-        # We're going to use a markdown link here
-        elif field == "privacy_policy":
-            rows.append(f"[Here]({value})")
+        hostname = urlparse(url).hostname
+        return f"[{hostname}]({url})"
 
-        # Handle author name
-        elif field == "owner":
-            # Assuming github url
-            author_name = value.split("/")
-            rows.append(f"[@{author_name[-1]}]({value})")
+    @staticmethod
+    def _create_country_column(country_code):
+        country_name = pycountry.countries.get(alpha_2=country_code).name
+        # https://stackoverflow.com/a/42235254
+        flag = (lambda s, e: chr(ord(s.upper()) - 0x41 + 0x1F1E6) +
+                             chr(ord(e.upper()) - 0x41 + 0x1F1E6)
+                )(*list(country_code))
+
+        return f"{flag} {country_name}"
+
+    @staticmethod
+    def _create_status_column(status_dict):
+        if not status_dict:
+            return ""
+
+        status_url = status_dict["url"]
+        display_content = status_dict["display_content"]
+
+        if status_dict.get("display_content_is_image"):
+            fallback = status_dict["display_content_image_fallback"]
+
+            return f"[![{fallback}]({display_content})]({status_url})"
         else:
-            rows.append(value)
+            return f"[{display_content}]({status_url})"
 
-    table_data.extend(rows)
+    @staticmethod
+    def _create_modified_column(is_modified, source):
+        if is_modified:
+            return f"[Yes]({source})"
+        return f"No"
+
+    @staticmethod
+    def _create_privacy_policy_column(privacy):
+        return f"[Here]({privacy})"
+
+    @staticmethod
+    def ddos_protection(ddos):
+        return f"{ddos}"
+
+    @staticmethod
+    def _create_owner_column(owner):
+        author_name = owner.split("/")
+        return f"[@{author_name[-1]}]({owner})"
+
+
+class MDInstanceListBuilder:
+    def __init__(self, instance_list_config):
+        self.config = instance_list_config
+        self.md = MdUtils(file_name='Invidious-Instances.md')
+        self.builder = ColumnBuilder()
+
+    def _generate_heading(self):
+        self.md.new_header(level=1, title='Public Instances')
+        self.md.new_paragraph("Uptime History: [uptime.invidious.io](https://uptime.invidious.io)")
+        self.md.new_paragraph("Instances API: [api.invidious.io](api.invidious.io)")
+
+    def _create_instance_tables(self):
+        # HTTPS
+        self.md.new_header(level=1, title='Instances list')
+        rows = ["Address", "Country", "Status", "Privacy policy", "DDos Protection / MITM", "Owner", "Modified"]
+        for instance in self.config["instances"]["https"]:
+            rows.extend(self._create_http_row(instance))
+        self.md.new_table(columns=7, rows=len(self.config["instances"]["https"]) + 1, text=rows, text_align='center')
+
+        self.md.new_line()
+
+        # Onion
+        self.md.new_header(level=1, title='Onion instances list')
+        rows = ["Address", "Country", "Associated clearnet instance", "Privacy policy", "Owner", "Modified"]
+        for instance in self.config["instances"]["onion"]:
+            rows.extend(self._create_onion_row(instance))
+        self.md.new_table(columns=6, rows=len(self.config["instances"]["onion"]) + 1, text=rows, text_align='center')
+
+    def _create_prerequisite_list(self):
+        self.md.new_header(level=2, title='Prerequisites')
+        self.md.new_list(self.config["adding_instance"]["prerequisites"])
+        self.md.new_line()
+
+    def _create_direction_list(self):
+        self.md.new_header(level=2, title='Directions')
+        self.md.new_list(self.config["adding_instance"]["directions"], marked_with="1")
+        self.md.create_md_file()
+
+    def _create_http_row(self, instance):
+        return [
+            self.builder.route("url")(instance["url"]),
+            self.builder.route("country")(instance["country"]),
+            self.builder.route("status")(instance["status"]),
+            self.builder.route("privacy_policy")(instance["privacy_policy"]),
+            self.builder.route("ddos_mitm_protection")(instance["ddos_mitm_protection"]),
+            self.builder.route("owner")(instance["owner"]),
+            self.builder.route("is_modified")(instance["is_modified"], instance["source"])
+        ]
+
+    def _create_onion_row(self, instance):
+        return [
+            self.builder.route("url")(instance["url"]),
+            self.builder.route("country")(instance["country"]),
+            self.builder.route("associated_clearnet_instance")(instance["associated_clearnet_instance"]),
+            self.builder.route("privacy_policy")(instance["privacy_policy"]),
+            self.builder.route("owner")(instance["owner"]),
+            self.builder.route("is_modified")(instance["is_modified"], instance["source"])
+        ]
+
+    def create(self):
+        self._generate_heading()
+        self.md.new_line()
+
+        self._create_instance_tables()
+        self.md.new_line()
+
+        # Instance adding directions and prerequisites
+        self.md.new_header(level=1, title='Adding your instance')
+        self._create_prerequisite_list()
+        self.md.new_line()
+        self._create_direction_list()
+
+        self.md.create_md_file()
 
 
 with open("instances.yaml") as configuration_yaml:
     data = yaml.safe_load(configuration_yaml)
 
-instance_list = data["instances"]
-
-# Initial information
-md_instance_list = MdUtils(file_name='Invidious-Instances.md')
-md_instance_list.new_header(level=1, title='Public Instances')
-md_instance_list.new_paragraph("Uptime History: [uptime.invidious.io](https://uptime.invidious.io)")
-md_instance_list.new_paragraph("Instances API: [api.invidious.io](api.invidious.io)")
-
-
-# Clearnet instances
-md_instance_list.new_header(level=1, title='Instances list')
-table_data = ["Address", "Country", "Status", "Privacy policy", "DDos Protection / MITM", "Owner", "Modified"]
-for instance_data in instance_list["https"]:
-    create_table(table_data, instance_data)
-
-md_instance_list.new_line()
-md_instance_list.new_table(columns=7, rows=len(instance_list["https"]) + 1, text=table_data, text_align='center')
-
-
-# Onion instances
-md_instance_list.new_header(level=1, title='Tor onion instances list')
-table_data = ["Address", "Country", "Associated clearnet instance", "Privacy policy", "Owner", "Modified"]
-for instance_data in instance_list["onion"]:
-    create_table(table_data, instance_data) 
-
-md_instance_list.new_line()
-md_instance_list.new_table(columns=6, rows=len(instance_list["onion"]) + 1, text=table_data, text_align='center')
-
-
-# Instance adding directions and prerequisites
-md_instance_list.new_header(level=1, title='Adding your instance')
-
-# Prerequisites
-md_instance_list.new_header(level=2, title='Prerequisites')
-md_instance_list.new_list(data["adding_instance"]["prerequisites"])
-md_instance_list.new_line()
-
-# Directions
-md_instance_list.new_header(level=2, title='Directions')
-md_instance_list.new_list(data["adding_instance"]["directions"], marked_with="1")
-md_instance_list.create_md_file()
+md_builder = MDInstanceListBuilder(data)
+md_builder.create()
